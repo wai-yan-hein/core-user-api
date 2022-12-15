d55f038e59b933c6a1d59c2891df362bb2bc4454
wai-yan <waiyanhain215@gmail.com>
2022-12-15 14:00:08 +0630

    w

--- cloud.sql
+++ cloud.sql
@@ -1,3 +1,8 @@
 alter table `department` 
 add column `acc_queue` varchar(255) null after `inv_queue`,
 change column `queue_name` `inv_queue` varchar(255) null default null ;
+
+set sql_safe_updates=0;
+update menu
+set menu_class ='Inventory'
+where menu_class='Report'
