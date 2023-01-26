11e65b39126e2e31ab73546b22e72801ece998cb
wai-yan <waiyanhain215@gmail.com>
2023-01-26 12:47:29 +0630

    w

--- src/main/java/cv/user/api/entity/Department.java
+++ src/main/java/cv/user/api/entity/Department.java
@@ -5,9 +5,11 @@ import lombok.Data;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Data
 @Entity
+@Table(name = "department")
 public class Department {
     @Id
     @Column(name = "dept_id")
